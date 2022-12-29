0192ac9f3bc19adb83bd6a3042fb036321e9b378
Yuki.kaco <a0936660023@gmail.com>
2022-12-29 18:06:06 +0800

    Player Stats With UI

---
+++ Assets/Scripts/PlayerHUD.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHUD : MonoBehaviour
+{
+    [SerializeField] private Text currentHealthText;
+    [SerializeField] private Text maxHealthText;
+
+    public void UpdateHealth(int currentHealth, int maxHealth)
+    {
+        currentHealthText.text = currentHealth.ToString();
+        maxHealthText.text = maxHealth.ToString();
+    }
+}
