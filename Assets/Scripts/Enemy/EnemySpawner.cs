e42544fc0415dac4391ff98777af4ebc50f0b400
Yuki.kaco <a0936660023@gmail.com>
2023-01-01 17:55:32 +0800

    Enemy Spawning

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private Transform[] spawners;
+
+    [SerializeField] private GameObject target;
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            SpawnTarget();
+        }
+    }
+
+    private void SpawnTarget()
+    {
+        int randomInt = Random.RandomRange(1, spawners.Length);
+        Transform randomSpawner = spawners[randomInt];
+
+        Instantiate(target, randomSpawner.position, randomSpawner.rotation);
+    }
+}
--- README.md
+++ README.md
@@ -8,3 +8,19 @@ not do:
 17 Equipping Weapons
 
 18 Weapon Swapping
+
+25 Low Poly Zombie Model In Blender - FPS Game With Unity & Blender
+
+26 Coloring The Zombie In Blender - FPS Game With Unity & Blender
+
+27 Rigging The Zombie In Blender (with IKs) - FPS Game With Unity & Blender
+
+28 Animating The Zombie In Blender - FPS Game With Unity
+
+29 Enemy Zombie AI In Unity - FPS Game With Unity & Blender
+
+30 Adding Animations To The Zombie - FPS Game With Unity & Blender
+
+31 Enemy Zombie Stats - FPS Game With Unity & Blender
+
+32 Zombie Attack - FPS Game With Unity & Blender
